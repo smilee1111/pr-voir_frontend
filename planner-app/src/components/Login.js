5bb003188503010944e99df36f60d089a95e16b7
smilee1111 <muskankc2005@gmail.com>
2025-02-05 09:33:08 +0545

    arrange file structure and make login page

---
+++ planner-app/src/components/Login.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "../style/Login.css";// Create this file for styling
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <h2>Login</h2>
+        <label>Username</label>
+        <input
+          type="text"
+          placeholder="Enter username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label>Password</label>
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <p className="forgot-password">Forget Password?</p>
+
+        <button type="submit">Login</button>
+
+        <p className="signup-text">
+          Not a Member? <a href="#">Signup</a>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
